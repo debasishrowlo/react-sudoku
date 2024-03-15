ab897b1742fe8929e2699fc60dc5613eb91f5a1b
kraytonian <creightonchingarande@gmail.com>
2024-03-14 22:52:28 -0700

    Row and Column array working need to do grid array

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import classnames from "classnames"
+import { start } from "repl"
+
 
 const App = () => {
   const [grid, setGrid] = useState([
@@ -14,12 +16,15 @@ const App = () => {
     5, 0, 8, 9, 2, 1, 7, 0, 4,
   ])
   // const [solution, setSolution] = useState<{ [key:number]: number[] }>({})
-  const [solution, setSolution] = useState<{ [key:number]: number[] }>(
-    {"3":[1],"4":[3],"8":[1],"12":[7],"13":[5],"14":[7],"15":[9],"16":[5],"17":[5],"22":[7],"23":[9],"24":[6],"25":[5],"26":[8],"27":[2],"30":[6],"32":[9],"33":[7],"34":[6],"36":[2],"37":[9],"40":[2],"42":[5],"44":[7],"45":[4],"46":[6],"47":[7],"48":[3],"49":[5],"50":[8],"52":[3],"53":[7],"55":[3],"56":[1],"59":[8],"60":[3],"62":[9],"63":[2],"64":[5],"69":[3],"71":[7],"73":[2],"79":[1]}
+  const [solution, setSolution] = useState<{ [key: number]: number[] }>(
+    { "3": [1], "4": [3], "8": [1], "12": [7], "13": [5], "14": [7], "15": [9], "16": [5], "17": [5], "22": [7], "23": [9], "24": [6], "25": [5], "26": [8], "27": [2], "30": [6], "32": [9], "33": [7], "34": [6], "36": [2], "37": [9], "40": [2], "42": [5], "44": [7], "45": [4], "46": [6], "47": [7], "48": [3], "49": [5], "50": [8], "52": [3], "53": [7], "55": [3], "56": [1], "59": [8], "60": [3], "62": [9], "63": [2], "64": [5], "69": [3], "71": [7], "73": [2], "79": [1] }
   )
-  const [activeCell, setActiveCell] = useState<number|null>(null)
 
-  const changeNumber = (num:number) => {
+
+
+  const [activeCell, setActiveCell] = useState<number | null>(null)
+
+  const changeNumber = (num: number) => {
     if (activeCell === null) { return }
 
     const nums = solution[activeCell]
@@ -56,7 +61,7 @@ const App = () => {
     }
   }
 
-  const selectCell = (cellIndex:number) => {
+  const selectCell = (cellIndex: number) => {
     if (grid[cellIndex] !== 0) {
       return
     }
@@ -65,14 +70,63 @@ const App = () => {
   }
 
   const isValid = () => {
-    // for(const girdItem in grid){
-    //   if(girdItem === activeCell.toString()){
-        
+    let row: number[] = []
+    let col: number[] = []
+    let gridArray = []
+
+    const getRow = () => {
+      const rowNum = Math.floor(activeCell / 9)
+      const start = rowNum * 9
+      const end = start + 9
+      row = grid.slice(start, end)
+      console.log("row", row)
+
+    }
+
+    const getCol = () => {
+      const start = activeCell % 9
+      const end = start + 72
+      for (let i = start; i <= end; i += 9) {
+        col.push(grid[i])
+      }
+      console.log("col", col)
+    }
+
+    const getBigSquare = () => {
+      console.log(activeCell)
+      console.log("rowNum", Math.floor(activeCell / 9))
+      console.log("colNum", activeCell % 9)
+      // get the row and col of active cell
+
+      // convert the small square to big square coord
+
+      // list all small sqaure values inside big square
+
+      // convert small sqaure coord to index
+    }
+
+    //old but
+    // const getCol = () => {
+    //   const colNum = activeCell % 9
+    //   console.log("col", colNum)
+    //   let currentCol = colNum;
+    //   for (let i = 0; i < 9; i++) {
+    //     console.log("col", currentCol, grid[currentCol])
+    //     currentCol = currentCol + 9
     //   }
     // }
 
-    
+    console.log(`############ validation start #############`)
+
+    if (activeCell) {
+      getRow()
+      getCol()
+      getBigSquare()
+    }
+
+    console.log(`############ validation end #############`)
   }
+  isValid()
 
   const isComplete = () => {
     const rows = 9
@@ -99,12 +153,12 @@ const App = () => {
       <div className="flex items-center">
         <div className="flex flex-wrap w-[500px]">
           {grid.map((sga, i) => {
-            const isHint = sga !== 0  
+            const isHint = sga !== 0
             const isActive = activeCell === i
             const cellsPerRow = 9
 
             let value = sga
-            let solutions:number[] = []
+            let solutions: number[] = []
             if (solution[i]) {
               solutions = solution[i]
             }
@@ -124,7 +178,7 @@ const App = () => {
                   "text-white": !isActive,
                   "bg-white/10": isHint,
                 })}
-                style={{ width: `${100 / cellsPerRow}%`}}
+                style={{ width: `${100 / cellsPerRow}%` }}
                 onClick={() => selectCell(i)}
                 key={i}
               >
@@ -158,7 +212,7 @@ const App = () => {
               return null
             }
 
-            let solutions:number[] = []
+            let solutions: number[] = []
             if (solution[activeCell]) {
               solutions = solution[activeCell]
             }
@@ -166,8 +220,8 @@ const App = () => {
             const isSelected = solutions.includes(key)
 
             return (
-              <button 
-                type="button" 
+              <button
+                type="button"
                 className={classnames("w-[50px] mt-1.5 block first:mt-0 border border-white rounded aspect-square", {
                   "bg-white text-indigo-950 ": isSelected,
                   "text-white": !isSelected,
@@ -175,7 +229,7 @@ const App = () => {
                 onClick={() => changeNumber(key)}
                 key={key}
               >
-                <span 
+                <span
                   className={classnames({ "opacity-0": key === 0 })}
                 >
                   {key}
